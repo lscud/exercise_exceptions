e0337cf7403d6e9620106f5c4d9243ab687b5051
Luis Antonio <luisscudeler@yahoo.com.br>
2023-02-05 10:57:04 -0300

    Part1 - sem delegacao (ruim)

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Infore os dados da conta: ");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine(); //massete para consumir o enter que foi dado no nextInt acima. Esse enter fica no buffer por isso precisamos consumi-lo com esse comando antes de dar nestLine();
+		String name = sc.nextLine();
+		System.out.print("Saldo Inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de Saque: ");
+		double limit = sc.nextDouble();
+		
+		Account acc = new Account(number, name, balance, limit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		if (amount > acc.getWithdrawLimit()) {
+			System.out.println("Erro de saque: A quantia excede o limite de saque");
+		}
+		else if (amount > acc.getBalance()) {
+			System.out.println("Erro de saque: Saldo Insuficiente");
+		}
+		else {	
+			acc.withdraw(amount);
+			System.out.printf("Novo salto: %.2f%n", acc.getBalance());
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author luiss
+ *
+ */
+module exercicio_Tratamento_exceções {
+}
